7751a460bfcaaee9b774c81d8f885e0692938c1a
ml9951 <ml9951@rit.edu>
2017-10-16 13:41:55 -0400

    Updating train script to work for spacenet/AIGH labels

--- train.py
+++ train.py
@@ -1,28 +1,21 @@
-import os
-import cv2
-import torch
-import numpy as np
-import datetime
-from torch.multiprocessing import Pool
+#!/usr/bin/env python
 
+import torch, cv2
+from Dataset import RasterDataset as Dataset
+import os, numpy as np, datetime
 from darknet import Darknet19
-
-from datasets.pascal_voc import VOCDataset
-import utils.yolo as yolo_utils
 import utils.network as net_utils
 from utils.timer import Timer
 import cfgs.config as cfg
+import pdb
+from torch.multiprocessing import Pool
+from torch.utils import data
+from torch.autograd import Variable
+from utils.im_transform import imcv2_recolor
 
-try:
-    from pycrayon import CrayonClient
-except ImportError:
-    CrayonClient = None
-
-
-# data loader
-imdb = VOCDataset(cfg.imdb_train, cfg.DATA_DIR, cfg.train_batch_size,
-                  yolo_utils.preprocess_train, processes=2, shuffle=True, dst_size=cfg.inp_size)
-print('load data succ...')
+def detection_collate(batch):
+    inputs, boxes, labels, dontcares = zip(*batch)
+    return torch.stack(inputs, 0), list(boxes), list(labels), list(dontcares)
 
 net = Darknet19()
 # net_utils.load_net(cfg.trained_model, net)
@@ -39,85 +32,72 @@ start_epoch = 0
 lr = cfg.init_learning_rate
 optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=cfg.momentum, weight_decay=cfg.weight_decay)
 
-# tensorboad
-use_tensorboard = cfg.use_tensorboard and CrayonClient is not None
-# use_tensorboard = False
+use_tensorboard = False
 remove_all_log = False
-if use_tensorboard:
-    cc = CrayonClient(hostname='127.0.0.1')
-    if remove_all_log:
-        print('remove all experiments')
-        cc.remove_all_experiments()
-    if start_epoch == 0:
-        try:
-            cc.remove_experiment(cfg.exp_name)
-        except ValueError:
-            pass
-        exp = cc.create_experiment(cfg.exp_name)
-    else:
-        exp = cc.open_experiment(cfg.exp_name)
-
-batch_per_epoch = imdb.batch_per_epoch
+batch_size = 16
+batch_per_epoch = 32
 train_loss = 0
 bbox_loss, iou_loss, cls_loss = 0., 0., 0.
 cnt = 0
 t = Timer()
 step_cnt = 0
-for step in range(start_epoch * imdb.batch_per_epoch, cfg.max_epoch * imdb.batch_per_epoch):
-    t.tic()
-    # batch
-    batch = imdb.next_batch()
-    im = batch['images']
-    gt_boxes = batch['gt_boxes']
-    gt_classes = batch['gt_classes']
-    dontcare = batch['dontcare']
-    orgin_im = batch['origin_im']
 
-    # forward
-    im_data = net_utils.np_to_variable(im, is_cuda=True, volatile=False).permute(0, 3, 1, 2)
-    net(im_data, gt_boxes, gt_classes, dontcare)
+epochs = 100
 
-    # backward
-    loss = net.loss
-    bbox_loss += net.bbox_loss.data.cpu().numpy()[0]
-    iou_loss += net.iou_loss.data.cpu().numpy()[0]
-    cls_loss += net.cls_loss.data.cpu().numpy()[0]
-    train_loss += loss.data.cpu().numpy()[0]
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    cnt += 1
-    step_cnt += 1
-    duration = t.toc()
-    if step % cfg.disp_interval == 0:
-        train_loss /= cnt
-        bbox_loss /= cnt
-        iou_loss /= cnt
-        cls_loss /= cnt
-        print('epoch %d[%d/%d], loss: %.3f, bbox_loss: %.3f, iou_loss: %.3f, cls_loss: %.3f (%.2f s/batch, rest:%s)' % (
-            imdb.epoch, step_cnt, batch_per_epoch, train_loss, bbox_loss, iou_loss, cls_loss, duration,
-            str(datetime.timedelta(seconds=int((batch_per_epoch - step_cnt) * duration)))))
+def transform(im, boxes, labels):
+    im = imcv2_recolor(im)
+    im = cv2.resize(im, (416, 416))
+    boxes = ((boxes / 300) * 416).round()
+    return im, boxes, labels
 
-        if use_tensorboard and step % cfg.log_interval == 0:
-            exp.add_scalar_value('loss_train', train_loss, step=step)
-            exp.add_scalar_value('loss_bbox', bbox_loss, step=step)
-            exp.add_scalar_value('loss_iou', iou_loss, step=step)
-            exp.add_scalar_value('loss_cls', cls_loss, step=step)
-            exp.add_scalar_value('learning_rate', lr, step=step)
+dataset = Dataset('training_data', transform=transform)
 
-        train_loss = 0
-        bbox_loss, iou_loss, cls_loss = 0., 0., 0.
-        cnt = 0
-        t.clear()
+dataloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=detection_collate)
+batches_per_epoch = len(dataset) / batch_size
 
-    if step > 0 and (step % imdb.batch_per_epoch == 0):
-        if imdb.epoch in cfg.lr_decay_epochs:
-            lr *= cfg.lr_decay
-            optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=cfg.momentum, weight_decay=cfg.weight_decay)
+for epoch in range(epochs):
+    for step, (inputs, gt_boxes, gt_classes, dontcare) in enumerate(dataloader):
+        t.tic()
 
-        save_name = os.path.join(cfg.train_output_dir, '{}_{}.h5'.format(cfg.exp_name, imdb.epoch))
-        net_utils.save_net(save_name, net)
-        print('save model: {}'.format(save_name))
-        step_cnt = 0
+        inputs = Variable(inputs.cuda())
 
-imdb.close()
+        # forward
+        net(inputs, gt_boxes, gt_classes, dontcare)
+
+        # backward
+        loss = net.loss
+        bbox_loss += net.bbox_loss.data.cpu().numpy()[0]
+        iou_loss += net.iou_loss.data.cpu().numpy()[0]
+        cls_loss += net.cls_loss.data.cpu().numpy()[0]
+        train_loss += loss.data.cpu().numpy()[0]
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        cnt += 1
+        step_cnt += 1
+        duration = t.toc()
+        if step % cfg.disp_interval == 0:
+            train_loss /= cnt
+            bbox_loss /= cnt
+            iou_loss /= cnt
+            cls_loss /= cnt
+            print('epoch %d[%d/%d], loss: %.3f, bbox_loss: %.3f, iou_loss: %.3f, cls_loss: %.3f (%.2f s/batch)' % (
+                epoch, step, batches_per_epoch, train_loss, bbox_loss, iou_loss, cls_loss, duration))
+
+            train_loss = 0
+            bbox_loss, iou_loss, cls_loss = 0., 0., 0.
+            cnt = 0
+            t.clear()
+
+    if epoch in cfg.lr_decay_epochs:
+        lr *= cfg.lr_decay
+        optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=cfg.momentum, weight_decay=cfg.weight_decay)
+
+    save_name = os.path.join(cfg.train_output_dir, '{}_{}.h5'.format(cfg.exp_name, epoch))
+    net_utils.save_net(save_name, net)
+    print('save model: {}'.format(save_name))
+    step_cnt = 0
+
+
+
+
